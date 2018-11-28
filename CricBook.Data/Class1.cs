3d8535fdcb522248f0468e75f48dd872562df474
venupigilam <venupigilam@gmail.com>
2018-11-28 17:59:26 +0530

    test Edmx Prepared ( Entity Framework)

---
+++ CricBook.Data/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CricBook.Data
+{
+    public class Class1
+    {
+    }
+}
